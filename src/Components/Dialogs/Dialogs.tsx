7223885a09908bce6e099109ef299c5b3aa36087
alex170485 <alex.170485@icloud.com>
2020-12-03 16:57:32 +0300

    Changed component Dialogs

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -5,8 +5,30 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItem}>
+                <div className={styles.dialog}>
+                    Sasha
+                </div>
+                <div className={styles.dialog}>
+                    John
+                </div>
+                <div className={styles.dialog}>
+                    David
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>
+                    Hi
+                </div>
+                <div className={styles.message}>
+                    How are you?
+                </div
+                ><div className={styles.message}>
+                    YOYOYOY
+                </div>
+
+            </div>
 
         </div>
     )
