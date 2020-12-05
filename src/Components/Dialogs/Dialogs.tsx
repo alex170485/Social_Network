f4c7745216cfde33a62bc6820a9826085b44b12a
alex170485 <alex.170485@icloud.com>
2020-12-05 16:56:10 +0300

    Add NavLink to name users

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 
 
@@ -8,13 +9,13 @@ const Dialogs = () => {
         <div className={styles.dialogs}>
             <div className={styles.dialogsItem}>
                 <div className={styles.dialog}>
-                    Sasha
+                    <NavLink to = {'/dialogs/1'}>Sasha</NavLink>
                 </div>
                 <div className={styles.dialog}>
-                    John
+                    <NavLink to = {'/dialogs/2'}>John</NavLink>
                 </div>
                 <div className={styles.dialog}>
-                    David
+                    <NavLink to = {'/dialogs/3'}>David</NavLink>
                 </div>
             </div>
             <div className={styles.messages}>
