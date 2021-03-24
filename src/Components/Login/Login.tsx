9c841f1af0f3c67a4859d359f41b53a05f807f6d
alex170485 <alex.170485@icloud.com>
2021-03-24 14:44:00 +0300

    add Redux-form

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
 
-
-
-const Login = () => {
-    return <h1> login</h1>
+type formatDataType = {
+    login: string,
+    password: string,
+    rememberMe: boolean,
 
 }
+
+const Login = () => {
+    const onSubmit = (formData: any) => {
+console.log(formData)
+    }
+    return <div>
+    <h1>Login</h1>
+        <LoginReduxForm onSubmit = {onSubmit}/>
+    </div>
+
+}
+type LoginFormType = any
+
+
+
+const LoginForm = (props: LoginFormType) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'login'} name={'login'} component = {'input'}/>
+            </div>
+            <div>
+                <Field placeholder={'password'} name={'password'} component = {'input'}/>
+            </div>
+            <div>
+                <Field type="checkbox" name={'rememberMe'} component = {'input'}/>Remember my
+            </div>
+            <div><button>Sing in</button></div>
+        </form>
+    )
+}
+const LoginReduxForm = reduxForm({
+    // a unique name for the form
+    form: 'login'
+})(LoginForm)
 
 export default Login
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,6 +4,7 @@ import dialogsReducer from "./dialogs-reduser";
 import users_reducer from "./users_reducer";
 import authReducer from "./auth-reduser";
 import thunk from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
 
 
 /*созщдаем объект с ключами свойство: значение. Сюда вносятся все наши венки проекта*/
@@ -11,7 +12,8 @@ let RootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: users_reducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 export type RootStateReduxType = ReturnType<typeof RootReducer>
